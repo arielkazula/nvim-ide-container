a7b69e4e18faa88a68dde9d3cc62960107c6f0bb
Ariel Kazula <arielkazula@users.noreply.github.com>
2024-11-12 10:27:27 +0200

    Update settings.lua

--- lua/settings.lua
+++ lua/settings.lua
@@ -1,13 +1,14 @@
 -- settings.lua
-vim.o.number = true        -- Show line numbers
+vim.o.number = true -- Show line numbers
 vim.o.relativenumber = false -- Show relative line numbers
-vim.o.hlsearch = true      -- Highlight search results
-vim.o.expandtab = true     -- Convert tabs to spaces
-vim.o.shiftwidth = 4       -- Indentation width
-vim.o.tabstop = 4          -- Tab width
-vim.o.clipboard = 'unnamedplus'
+vim.o.hlsearch = true -- Highlight search results
+vim.o.expandtab = true -- Convert tabs to spaces
+vim.o.shiftwidth = 4 -- Indentation width
+vim.o.tabstop = 4 -- Tab width
+vim.o.clipboard = "unnamedplus"
 vim.o.textwidth = 80
 vim.o.wrapmargin = 0
 vim.o.colorcolumn = "80"
 vim.o.guicursor = "n-v-c-sm:block,i-ci-ve:ver25,r-cr-o:hor20"
-
+vim.cmd("colorscheme onedark")
+vim.opt.termguicolors = true
