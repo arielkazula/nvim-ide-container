812bb115f722ac31b4e7ec3d57fa76f5f8efed77
Ariel Kazula <arielkazula@users.noreply.github.com>
2024-11-20 14:20:09 +0200

    Update plugins.lua

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -185,24 +185,35 @@ return {
                 end,
         },
         {
-                "nvimdev/dashboard-nvim",
-                event = "VimEnter",
-                opts = function(_, opts)
-                        local logo = [[
-             _______     _                  _________ ______   _______
-            (  ___  )   | \    /\           \__   __/(  __  \ (  ____ \
-            | (   ) |   |  \  / /              ) (   | (  \  )| (    \/
-            | (___) |   |  (_/ /    _____      | |   | |   ) || (__
-            |  ___  |   |   _ (    (_____)     | |   | |   | ||  __)
-            | (   ) |   |  ( \ \               | |   | |   ) || (
-            | )   ( | _ |  /  \ \           ___) (___| (__/  )| (____/\
-            |/     \|(_)|_/    \/           \_______/(______/ (_______/
-            ]]
+                "folke/snacks.nvim",
+                opts = {
+                        dashboard = {
+                                preset = {
+                                        header = [[
+██╗  ██╗ █████╗ ███████╗██╗   ██╗██╗      █████╗     ██╗   ██╗██╗███╗   ███╗
+██║ ██╔╝██╔══██╗╚══███╔╝██║   ██║██║     ██╔══██╗    ██║   ██║██║████╗ ████║
+█████╔╝ ███████║  ███╔╝ ██║   ██║██║     ███████║    ██║   ██║██║██╔████╔██║
+██╔═██╗ ██╔══██║ ███╔╝  ██║   ██║██║     ██╔══██║    ╚██╗ ██╔╝██║██║╚██╔╝██║
+██║  ██╗██║  ██║███████╗╚██████╔╝███████╗██║  ██║     ╚████╔╝ ██║██║ ╚═╝ ██║
+╚═╝  ╚═╝╚═╝  ╚═╝╚══════╝ ╚═════╝ ╚══════╝╚═╝  ╚═╝      ╚═══╝  ╚═╝╚═╝     ╚═╝
 
-                        logo = string.rep("\n", 8) .. logo .. "\n\n"
-                        opts.config.header = vim.split(logo, "\n")
-                        opts.theme = "doom"
-                end,
+ ]],
+        -- stylua: ignore
+        ---@type snacks.dashboard.Item[]
+        keys = {
+          { icon = " ", key = "f", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
+          { icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
+          { icon = " ", key = "g", desc = "Find Text", action = ":lua Snacks.dashboard.pick('live_grep')" },
+          { icon = " ", key = "r", desc = "Recent Files", action = ":lua Snacks.dashboard.pick('oldfiles')" },
+          { icon = " ", key = "c", desc = "Config", action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})" },
+          { icon = " ", key = "s", desc = "Restore Session", section = "session" },
+          { icon = " ", key = "x", desc = "Lazy Extras", action = ":LazyExtras" },
+          { icon = "󰒲 ", key = "l", desc = "Lazy", action = ":Lazy" },
+          { icon = " ", key = "q", desc = "Quit", action = ":qa" },
+        },
+                                },
+                        },
+                },
         },
         {
                 "kdheepak/lazygit.nvim",
