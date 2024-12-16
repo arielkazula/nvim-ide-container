2dc110ecdecf55e08aa165361fd9b4762d340155
Ariel Kazula <arielkazula@users.noreply.github.com>
2024-12-16 11:35:09 +0200

    Update plugins.lua

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -15,22 +15,6 @@ return {
         -- Neovim LSP configurations
         { "neovim/nvim-lspconfig" },
 
-        -- Completion and Snippets
-
-        {
-                "hrsh7th/nvim-cmp",
-                dependencies = {
-                        "L3MON4D3/LuaSnip",
-                        "saadparwaiz1/cmp_luasnip",
-                },
-                opts = function(_, opts)
-                    local cmp = require("cmp")
-                    opts.experimental = opts.experimental or {}
-                    opts.experimental.ghost_text = false -- Disable ghost text
-                    return opts
-                end,
-        },
-
         { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
         -- Add more Treesitter parsers
         {
@@ -52,8 +36,6 @@ return {
         },
 
         { "nvim-treesitter/playground" },
-        -- Telescope for fuzzy searching
-        { "nvim-telescope/telescope.nvim", dependencies = { "nvim-lua/plenary.nvim" } },
 
         -- Neogen for generating Doxygen comments
         {
@@ -365,23 +347,43 @@ return {
                 },
         },
         {
-                "rachartier/tiny-code-action.nvim",
-                dependencies = {
-                        { "nvim-lua/plenary.nvim" },
-                        { "nvim-telescope/telescope.nvim" },
-                },
-                event = "LspAttach",
-                config = function()
-                        require("tiny-code-action").setup({
-                                telescope_opts = {
-                                        layout_strategy = "horizontal",
-                                        layout_config = {
-                                                width = 0.6,
-                                                height = 0.6,
-                                                preview_cutoff = 1,
-                                        },
+                "saghen/blink.cmp",
+                -- optional: provides snippets for the snippet source
+                dependencies = "rafamadriz/friendly-snippets",
+
+                ---@module 'blink.cmp'
+                ---@type blink.cmp.Config
+                opts = {
+                        -- 'default' for mappings similar to built-in completion
+                        -- 'super-tab' for mappings similar to vscode (tab to accept, arrow keys to navigate)
+                        -- 'enter' for mappings similar to 'super-tab' but with 'enter' to accept
+                        -- see the "default configuration" section below for full documentation on how to define
+                        -- your own keymap.
+                        keymap = { preset = "super-tab" },
+
+                        appearance = {
+                                -- Sets the fallback highlight groups to nvim-cmp's highlight groups
+                                -- Useful for when your theme doesn't support blink.cmp
+                                -- will be removed in a future release
+                                use_nvim_cmp_as_default = true,
+                        }, -- Add
+                        completion = {
+                                ghost_text = {
+                                        enabled = false, -- Ensure ghost text is explicitly disabled
                                 },
-                        })
-                end,
+                        },
+                        windows = {
+                                ghost_text = {
+                                        enabled = false, -- Double-check disabling in windows settings
+                                },
+                        },
+
+                        diagnostics = {
+                                display_virtual_text = false, -- Disable virtual text
+                        },
+                },
+                -- allows extending the providers array elsewhere in your config
+                -- without having to redefine it
+                opts_extend = { "sources.default" },
         },
 }
